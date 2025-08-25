5d714d1c05979912538d0f1dbc27030abcd117a7
coderrr1ck <coderrr1ck@gmail.com>
2025-08-25 14:55:40 +0530

    pushing updated code

--- patientMicroservice/src/main/java/com/coderrr1ck/patientMicroservice/DTOs/CreatePatientValidationGroup.java
+++
@@ -1,6 +0,0 @@
-package com.coderrr1ck.patientMicroservice.DTOs;
-
-public interface CreatePatientValidationGroup {
-}
-//this is used for creating validation group,
-// to reuse same dto for based on group based validation
